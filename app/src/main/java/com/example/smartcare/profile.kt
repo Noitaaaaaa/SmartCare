7e406bcd30bf123bb49bae3af14fa8f9b31143b1
D4rkfall <jpmolera99@gmail.com>
2026-09-03 13:25:04 +0800

    profile and register

--- app/src/main/java/com/example/smartcare/profile.kt
+++ app/src/main/java/com/example/smartcare/profile.kt
@@ -1,10 +1,32 @@
 package com.example.smartcare
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 
 fun Profile() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(18.dp)
+    ){
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = ("Profile"))
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+    }
 
 
 }
--- app/src/main/java/com/example/smartcare/register.kt
+++ app/src/main/java/com/example/smartcare/register.kt
@@ -1,2 +1,30 @@
 package com.example.smartcare
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun Register() {
+
+ Column(
+     modifier = Modifier.fillMaxSize()
+         .padding(18.dp)
+ ){
+
+     Spacer(modifier = Modifier.height(32.dp))
+
+     Text( text = ("Register"), style = MaterialTheme.typography.headlineSmall)
+
+     Spacer(modifier = Modifier.height(32.dp))
+
+ }
+}
